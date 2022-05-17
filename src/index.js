cfe660ce0b62dad916cc3744131a023db6dad1a2
ONJoseph <on.josephinfo@gmail.com>
2022-05-17 23:02:30 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import logo from './logoImg.jpg';
 import getShows from './getMoviez.js';
 
 //Add logo img
+
 const element = document.querySelector('.logoContainer');
 const mylogo = new Image();
 mylogo.className = 'logo';
