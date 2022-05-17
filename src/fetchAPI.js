ab1de784d02c35d59ad7b14b2ad299eec7892521
ONJoseph <on.josephinfo@gmail.com>
2022-05-17 22:43:17 +0100

    add comments functionality

---
+++ src/fetchAPI.js
@@ -0,0 +1,11 @@
+export const movieID = [];
+
+export function getMoviez() {
+  fetch("https://api.tvmaze.com/shows")
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < 20; i += 1) {
+        movieID.push(data[i]);
+      }
+    });
+}
