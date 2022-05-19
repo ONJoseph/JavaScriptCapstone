62747aaac52f4afd9d56d1deb82e0c860d0e7a4d
ONJoseph <on.josephinfo@gmail.com>
2022-05-19 16:01:38 +0100

    add updateCounter and test

---
+++ src/__mocks__/updateCounter.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    comment: 'Its Awesome',
+    creation_date: '2022-04-05',
+    username: 'Joseph',
+  },
+  {
+    comment: 'This is really wonderful!',
+    creation_date: '2022-03-09',
+    username: 'Aldo',
+  },
+  {
+    comment: 'Wondering what space looks like! Watch this!!',
+    creation_date: '2022-05-10',
+    username: 'Excel',
+  },
+  {
+    comment: 'This is great!',
+    creation_date: '2022-04-05',
+    username: 'Dan',
+  },
+  {
+    comment: 'I am going to see this awesome movie again!!',
+    creation_date: '2022-03-08',
+    username: 'Mavin',
+  },
+];
+
+function updateCounter() {
+  const allComments = document.getElementsByClassName('single-comment');
+  const counter = allComments.length;
+  const commentsCounter = document.getElementsByClassName('comments-count');
+  commentsCounter[0].innerText = `(${counter})`;
+  const updatedCounter = commentsCounter;
+  return updatedCounter;
+}
+
---
+++ src/__tests__/updateCounter.test.js
@@ -0,0 +1,15 @@
+import updateCounter from './comments.js';
+
+jest.mock('./comments.js');
+
+describe('Testing updateCounter function', () => {
+  test('Check that the number of elements added is one', () => {
+    const result = updateCounter();
+    expect(result).toBe(1);
+  });
+
+  test('Check that the function returns value different from null', () => {
+    const result = updateCounter();
+    expect(result).not.toBeNull();
+  });
+});
